64f8fb03321d690b006a3add279d3c58ca48730b
Michel Daviot <michel.daviot@colisweb.com>
2022-01-04 14:43:24 +0100

    feat: publish for scala3 and pprint 0.7

--- build.sbt
+++ build.sbt
@@ -1,13 +1,13 @@
 name := "approvals-scala"
 
-crossScalaVersions := List("2.12.13", "2.13.5")
+crossScalaVersions := List("2.12.15", "2.13.4", "3.1.0")
 
 libraryDependencies ++= Seq(scalaTest, approvalsJava, pprint)
 
 resolvers += Resolver.mavenLocal
 
-lazy val pprint = "com.lihaoyi" %% "pprint" % "0.6.4"
-lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.7"
+lazy val pprint = "com.lihaoyi" %% "pprint" % "0.7.1"
+lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.10"
 lazy val approvalsJava = "com.colisweb" % "approvals-java" % "0.13.1"
 ThisBuild / pushRemoteCacheTo := Some(
   MavenCache("local-cache", baseDirectory.value / sys.env.getOrElse("CACHE_PATH", "sbt-cache"))
--- src/test/scala/Demo.scala
+++ src/test/scala/Demo.scala
@@ -1,9 +1,20 @@
 import com.github.writethemfirst.Approbation
-import com.github.writethemfirst.approvals.approvers.Approver
 import org.scalatest.flatspec.FixtureAnyFlatSpec
 
 class Demo extends FixtureAnyFlatSpec with Approbation {
-  it should "approve demo" in { approver: Approver =>
+  it should "approve demo" in { approver =>
     approver.verify("demo")
   }
+
+  it should "approve a case class" in { approver =>
+    approver.verify(Seq(
+      Person("John Doe", Address("calle Alvarez", 166)),
+      Person("John Doe", Address("calle Alvarez", 166)),
+      Person("John Doe", Address("calle Alvarez", 166)),
+    ))
+  }
 }
+
+final case class Person(name: String, address: Address)
+
+final case class Address(street: String, number: Int)
